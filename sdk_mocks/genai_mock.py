c6d1c392ad830636027d3136e126e7ca657ba473
shumizu418128 <84061748+shumizu418128@users.noreply.github.com>
2025-09-28 11:31:34 +0900

    change wrong function name

--- sdk_mocks/genai_mock.py
+++ sdk_mocks/genai_mock.py
@@ -2,9 +2,9 @@ from unittest.mock import patch, Mock
 from google import genai
 
 @patch('genai.Client')
-def test_gemini_client(mock_client):
+def mock_gemini_client(mock_client):
     mock_client.return_value = Mock(spec=genai.Client)
 
 @patch("genai.models.generate_content")
-def test_gemini_generate_content(mock_generate_content):
+def mock_gemini_generate_content(mock_generate_content):
     mock_generate_content.return_value = Mock(spec=genai.models.generate_content)
