2d2a125c49218f6e0ae0161e41442b3ed9a884c7
shumizu418128 <84061748+shumizu418128@users.noreply.github.com>
2025-09-28 11:37:51 +0900

    linter

--- sdk_mocks/genai_mock.py
+++ sdk_mocks/genai_mock.py
@@ -1,9 +1,12 @@
-from unittest.mock import patch, Mock
+from unittest.mock import Mock, patch
+
 from google import genai
 
-@patch('genai.Client')
+
+@patch("genai.Client")
 def mock_gemini_client(mock_client):
     mock_client.return_value = Mock(spec=genai.Client)
+
 
 @patch("genai.models.generate_content")
 def mock_gemini_generate_content(mock_generate_content):
